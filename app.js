2c5fe50c8f532e7b6451b89cc73b87bb00be978c
OH-JOO-YEONG <brb1111@naver.com>
2021-02-01 20:29:43 +0900

    canvas api applicated

---
+++ app.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        // 경로를 만든다.
+        ctx.beginPath(); // 경로생성
+        ctx.moveTo(x, y); // 선 시작 좌표
+    } else {
+        // 그린다.
+        ctx.lineTo(x, y); // 선 끝 좌표
+        ctx.stroke(); // 선그리기
+    }
+}
+
+function onMouseLeave(event) {
+    painting = false;
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+}
